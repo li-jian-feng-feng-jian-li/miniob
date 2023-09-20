9daf01a94ebf693679c9679b4d23a4eaab064a5f
lijianfeng <2659123632@qq.com>
2023-09-20 10:51:20 +0800

    finish drop function

---
+++ src/observer/sql/executor/drop_table_executor.h
@@ -0,0 +1,15 @@
+// @author ljf
+#pragma once
+
+#include "common/rc.h"
+
+class SQLStageEvent;
+
+class DropTableExecutor
+{
+public:
+  DropTableExecutor() = default;
+  virtual ~DropTableExecutor() = default;
+
+  RC execute(SQLStageEvent *sql_event);
+};
